4415389f36ffde1754bb14697a51012d88f88b07
Azusa UEZU <e095764@cat.local>
2012-04-18 17:05:15 +0900

    AOJ-0003

---
+++ 0003.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void)
+{
+  int n,a,b,c;
+  int i;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d %d %d\n",&a,&b,&c);
+    if(a*a+b*b==c*c || a*a+c*c==b*b || b*b+c*c==a*a)
+    {
+      printf("YES\n");
+    }
+    else
+    {
+      printf("NO\n");
+    }
+
+  }
+  return 0;
+}
